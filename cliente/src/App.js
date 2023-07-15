50bdd5cd844cd9e844c051972f9d2907747e1eea
Gabriel <[gabrielgamboav19@gmail.com]>
2023-07-15 03:56:07 -0600

    Se agrega la barra de navegacion

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import AuthForm from './AuthForm';
 import UserExercises from './UserExercises';
+import NavbarComponent from './NavbarComponent';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    // <AuthForm />
-    <UserExercises username={'usuario1'} ></UserExercises>
+    <Router>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/account" element={ <UserExercises username="Gabriel" /> } />
+      </Routes>
+    </Router>
   );
 }
 
+//<Route path="/routine" element={ /* Colocar el componente de rutina aquí */ } />
+//        <Route path="/user-routines" element={ /* Colocar el componente de rutinas de usuario aquí */ } />
 export default App;
---
+++ cliente/src/NavbarComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      {/* El logo de la app que lleva al componente de rutina */}
+      <Navbar.Brand as={Link} to="/routine">
+        <img
+          alt=""
+          src="/logo.png" // Cambia esto por la ruta a tu imagen de logo
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        FIT 
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      {/* Sección de navegación a la derecha */}
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          {/* Enlace a la cuenta del usuario */}
+          <Nav.Link as={Link} to="/account">Account</Nav.Link>
+
+          {/* Enlace a las rutinas del usuario */}
+          <Nav.Link as={Link} to="/user-routines">User Routines</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
